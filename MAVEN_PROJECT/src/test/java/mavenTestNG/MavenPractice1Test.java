80c183d7255b245e2651351327640b94a693b9a9
DELL <DELL@DESKTOP-5N7435V>
2022-07-19 11:43:56 +0530

    first commit

---
+++ MAVEN_PROJECT/src/test/java/mavenTestNG/MavenPractice1Test.java
@@ -0,0 +1,21 @@
+package mavenTestNG;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice1Test {
+@Test
+public void maven1Test() {
+	System.out.println("Test Case 1");
+}
+
+@Test(priority=1)
+public void maven2Test() {
+	System.out.println("Test Case 2");
+}
+
+@Test
+public void maven3Test() {
+	System.out.println("Test Case 3");
+}
+}
+
---
+++ MAVEN_PROJECT/src/test/java/mavenTestNG/MavenPractice2Test.java
@@ -0,0 +1,22 @@
+package mavenTestNG;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice2Test {
+	@Test
+	public void maven4Test() {
+		System.out.println("Test Case 4");
+	}
+
+	@Test
+	public void maven5Test() {
+		System.out.println("Test Case 5");
+	}
+
+	@Test
+	public void maven6Test() {
+		System.out.println("Test Case 6");
+	}
+	}
+
+
